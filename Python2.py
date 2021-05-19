2bc7cf77488f3d8c856e69a7ddf244c14ec73852
Txbi2 <tobias.gretler@gretler.ch>
2021-05-19 09:59:39 +0200

    Verbesserung Python2.py

--- Python2.py
+++ Python2.py
@@ -1,15 +1,24 @@
 import random
 
-zufallszahl = random.randint(0,100)
-zahl = input("Gib eine zahl zwischen 0 und 100 ein ")
-Anzahlversuche = 0
 
-while zufallszahl != int(zahl):
-    Anzahlversuche=Anzahlversuche+1
-    if zufallszahl < int(zahl):
-        zahl = input("Gib eine kleinere Zahl ein ") 
-    else:
-        zahl = input("Gib eine grössere Zahl ein ")
+def neustart():
 
-print("Du hast die Zahl erraten")
-print(f"Du hast {Anzahlversuche} versuche gebraucht")
+    zufallszahl = random.randint(0, 100)
+    zahl = input("Gib eine zahl zwischen 0 und 100 ein ")
+    Anzahlversuche = 0
+
+    while zufallszahl != int(zahl):
+        Anzahlversuche = Anzahlversuche+1
+        if zufallszahl < int(zahl):
+            zahl = input("Gib eine kleinere Zahl ein ")
+        else:
+            zahl = input("Gib eine grössere Zahl ein ")
+
+    print("Du hast die Zahl erraten")
+    print(f"Du hast {Anzahlversuche} versuche gebraucht")
+    wiederholen = input("Möchtest du nochmal spielen? Ja/Nein ")
+
+    if wiederholen == "Ja":
+        neustart()
+
+neustart()
